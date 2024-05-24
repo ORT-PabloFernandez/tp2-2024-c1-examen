9953a6d1bf55b7c318bf0639cdda5bab32bb7ede
Ivan Chueco Tendler <ivan.chueco.t@gmail.com>
2024-05-23 21:11:43 -0300

    Ivan Chueco Tendler

--- data/sales.js
+++ data/sales.js
@@ -1,12 +1,13 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
 async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
-    .collection(MOVIES)
+    .collection(SALES)
     .find({})
     .limit(pageSize)
     .skip(pageSize * page)
@@ -14,4 +15,85 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+async function getSaleById(saleId) {
+  try {
+    const connectiondb = await getConnection();
+    const saleFound = await connectiondb
+      .db(DATABASE)
+      .collection(SALES)
+      .findOne({ _id: new ObjectId(saleId) });
+
+    return saleFound;
+  } catch (err) {
+    return "No sale found by that id"
+  }
+}
+
+async function getSalesByLocation(storeLocation) {
+  const connectiondb = await getConnection();
+  const salesByLocation = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({ storeLocation: storeLocation })
+    .toArray();
+
+  return salesByLocation.length > 0 ? salesByLocation : "No sales found in that location";
+}
+
+async function getSalesByLocationPurchaseMethodAndCouponUsed(storeLocation, purchaseMethod, couponUsed) {
+  const connectiondb = await getConnection();
+  const salesFound = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({
+      $and: [
+        { storeLocation: storeLocation },
+        { purchaseMethod: purchaseMethod },
+        { couponUsed: couponUsed }
+      ]
+    })
+    .toArray();
+
+  return salesFound.length > 0 ? salesFound : "No sales found with those parameters";
+}
+
+async function getMostBoughtItems() {
+  const connectiondb = await getConnection();
+  const itemsSold = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({})
+    .toArray();
+
+  const listOfItems = itemsSold.map((object) => object.items.splice(0)).flat();
+
+  let allSoldItems = {};
+  listOfItems.forEach(item => {
+    allSoldItems[item.name] = allSoldItems[item.name] == null ? item.quantity : allSoldItems[item.name] + item.quantity;
+  });
+
+  const soldItemsSorted = Object.keys(allSoldItems).sort((a, b) => { return allSoldItems[b] - allSoldItems[a] })
+  const response = soldItemsSorted.map(itemName => {
+    return {
+      name: itemName,
+      quantity: allSoldItems[itemName],
+    }
+  })
+
+  return response.splice(0, 10);
+}
+
+async function getCustomersBySatisfaction() {
+  const connectiondb = await getConnection();
+  const customersBySatisfaction = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({})
+    .sort({ "customer.satisfaction": -1 })
+    .toArray();
+
+  const response = customersBySatisfaction.map((object) => object.customer);
+  return response;
+}
+
+export { getAllSales, getSaleById, getSalesByLocation, getSalesByLocationPurchaseMethodAndCouponUsed, getMostBoughtItems, getCustomersBySatisfaction };
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getAllSales, getSaleById, getSalesByLocation, getSalesByLocationPurchaseMethodAndCouponUsed, getMostBoughtItems, getCustomersBySatisfaction } from "../data/sales.js";
 
 const router = express.Router();
 
@@ -9,5 +9,31 @@ router.get("/", async (req, res) => {
 
   res.json(await getAllSales(pageSize, page));
 });
+
+router.get("/location", async (req, res) => {
+  const storeLocation = req.query.storeLocation;
+  res.json(await getSalesByLocation(storeLocation));
+});
+
+router.get("/customSearch", async (req, res) => {
+  const { storeLocation, purchaseMethod } = req.query;
+  const couponUsed = req.query.couponUsed.toLowerCase() === 'true' ? true : false;
+  res.json(await getSalesByLocationPurchaseMethodAndCouponUsed(storeLocation, purchaseMethod, couponUsed));
+});
+
+router.get("/mostBought", async (req, res) => {
+  res.json(await getMostBoughtItems());
+});
+
+router.get("/customers/satisfaction", async (req, res) => {
+  res.json(await getCustomersBySatisfaction());
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  res.json(await getSaleById(id));
+});
+
 
 export default router;
